5ee692ecbf6fe0fedc8e99c2dc2fa8737c52668a
HarryPerez <c-feperez@globalization-partners.com>
2021-11-10 19:47:46 -0300

    some fixes and new features

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -5,12 +5,13 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
-import { Button, IconButton } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { IconButton, Button, TextField, Modal, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Loader from "react-loader-spinner";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 
 import CustomMarker from "./components/CustomMarker";
 import mapStyles from "./mapStyles.json";
@@ -26,6 +27,17 @@ const StyledBackButton = styled(Button)`
   height: 36px;
   width: 85px;
 `;
+
+const HtmlTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "white",
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 220,
+    border: "1px solid #dadde9",
+  },
+}));
 
 const MyMapComponent = withScriptjs(
   withGoogleMap((props) => (
@@ -52,6 +64,8 @@ function Dashboard() {
   const navigate = useNavigate();
   const [location, setLocation] = useState();
   const [users, setUsers] = useState();
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
+  const [form, setForm] = useState({});
   const user = useSelector((state) => state.app.authUser);
 
   const saveUserLocation = useCallback(
@@ -126,6 +140,28 @@ function Dashboard() {
     location,
   ]);
 
+  const onSaveProfile = async () => {
+    setIsProfileModalOpen(false);
+    if (form.phone && form.description) {
+      await axios
+        .post(`${process.env.REACT_APP_BACKEND_URL}/auth/user/extrainfo`, {
+          withCredentials: true,
+          user,
+          phone: form.phone,
+          description: form.description,
+        })
+        .catch((error) => {
+          console.log("No se pudo guardar el perfil del usuario");
+        });
+    }
+  };
+
+  const changeValue = (e, field) => {
+    const newForm = form;
+    newForm[field] = e;
+    setForm(newForm);
+  };
+
   return (
     <div className="dashboard-container">
       {location ? (
@@ -134,11 +170,6 @@ function Dashboard() {
             async
             src={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}&libraries=geometry&callback=initMap`}
           ></script>
-          <div className="link-container">
-            <Link to="/" style={{ textDecoration: "none" }}>
-              <StyledBackButton variant="text">LOGOUT</StyledBackButton>
-            </Link>
-          </div>
           <div className="dashboard-dashboard-container">
             <div className="dashboard-map-container">
               <MyMapComponent
@@ -175,15 +206,91 @@ function Dashboard() {
           </div>
           <div className="profile-picture-container">
             {user && user.picture && (
-              <IconButton color="primary" aria-label="Tu perfil!">
-                <img
-                  className="profile-picture"
-                  alt="profile"
-                  src={user.picture}
-                />
-              </IconButton>
+              <HtmlTooltip
+                title={
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <StyledBackButton
+                      onClick={() => setIsProfileModalOpen(true)}
+                      color="primary"
+                    >
+                      PERFIL
+                    </StyledBackButton>
+                    <StyledBackButton
+                      onClick={() => navigate("/")}
+                      color="primary"
+                    >
+                      SALIR
+                    </StyledBackButton>
+                  </div>
+                }
+              >
+                <IconButton color="primary">
+                  <img
+                    className="profile-picture"
+                    alt="profile"
+                    src={user.picture}
+                  />
+                </IconButton>
+              </HtmlTooltip>
             )}
           </div>
+          <Modal
+            open={isProfileModalOpen}
+            onClose={() => setIsProfileModalOpen(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box
+              component="form"
+              noValidate
+              autoComplete="off"
+              sx={{
+                position: "absolute",
+                borderRadius: "12px",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: 400,
+                bgcolor: "background.paper",
+                border: "2px solid #000",
+                boxShadow: 24,
+                p: 4,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "400px",
+              }}
+            >
+              <div className="info-window-body">
+                <img
+                  style={{ marginBottom: "10px" }}
+                  className="profile-picture"
+                  alt="profile"
+                  src={user?.picture}
+                />
+                <span style={{ fontWeight: "bold" }}>{user?.fullName}</span>
+                <span>{user?.email}</span>
+              </div>
+              <TextField
+                required
+                value={user.phone}
+                label="Número de telefono"
+                type="phonenumber"
+                onChange={(e) => changeValue(e.target.value, "phone")}
+              />
+              <TextField
+                value={user.description}
+                onChange={(e) => changeValue(e.target.value, "description")}
+                required
+                label="Descripción"
+                multiline
+                rows={4}
+              />
+              <Button variant="contained" onClick={() => onSaveProfile()}>
+                Guardar
+              </Button>
+            </Box>
+          </Modal>
         </div>
       ) : (
         <div className="loader-container">
